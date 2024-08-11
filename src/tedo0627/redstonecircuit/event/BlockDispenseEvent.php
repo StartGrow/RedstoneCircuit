6aefa4113832a1e0335b31ac996780faf2cd0cbd
Lil Kev <kjolly544@gmail.com>
2024-08-10 21:19:49 -0400

    LATEST PMMP UPDATE

--- src/tedo0627/redstonecircuit/event/BlockDispenseEvent.php
+++ src/tedo0627/redstonecircuit/event/BlockDispenseEvent.php
@@ -5,17 +5,15 @@ namespace tedo0627\redstonecircuit\event;
 use pocketmine\block\Block;
 use pocketmine\event\block\BlockEvent;
 use pocketmine\event\Cancellable;
-use pocketmine\event\CancellableTrait;
 use pocketmine\item\Item;
 
 class BlockDispenseEvent extends BlockEvent implements Cancellable {
-    use CancellableTrait;
+    use \pocketmine\event\CancellableTrait; // Ensure correct trait namespace
 
     private Item $item;
 
     public function __construct(Block $block, Item $item) {
         parent::__construct($block);
-
         $this->item = $item;
     }
 
