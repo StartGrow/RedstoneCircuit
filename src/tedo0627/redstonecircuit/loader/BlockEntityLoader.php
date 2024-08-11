78f13207bd341cda9f989cf49e1b50dc96434b1c
Lil Kev <kjolly544@gmail.com>
2024-08-10 21:29:50 -0400

    LATEST PMMP UPDATE

--- src/tedo0627/redstonecircuit/loader/BlockEntityLoader.php
+++ src/tedo0627/redstonecircuit/loader/BlockEntityLoader.php
@@ -2,13 +2,21 @@
 
 namespace tedo0627\redstonecircuit\loader;
 
-use pocketmine\block\tile\TileFactory;
+use pocketmine\tile\TileFactory;
+use pocketmine\tile\TileType;
 
 class BlockEntityLoader extends Loader {
 
     private string $className;
     private array $saveNames;
 
+    /**
+     * BlockEntityLoader constructor.
+     *
+     * @param string $name The name of the loader.
+     * @param string $className The class name of the tile entity.
+     * @param array $saveNames The names used to save the tile entity.
+     */
     public function __construct(string $name, string $className, array $saveNames) {
         parent::__construct($name);
 
@@ -16,7 +24,10 @@ class BlockEntityLoader extends Loader {
         $this->saveNames = $saveNames;
     }
 
+    /**
+     * Registers the block entity (tile entity) with the TileFactory.
+     */
     public function load(): void {
-        TileFactory::getInstance()->register($this->className, $this->saveNames);
+        TileFactory::getInstance()->registerTile($this->className, $this->saveNames);
     }
 }
